0f62331dd833428a97ad8ef0d6031b4f13091d17
SpringZhang <38313380+SpringZhang1978@users.noreply.github.com>
2018-11-20 16:57:02 +0800

    Update SmartTalk.py

--- SmartTalk.py
+++ SmartTalk.py
@@ -11,7 +11,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 #https://github.com/SpringZhang1978/SmartTalk.git
-#本样例代码使用baidu在线语音识别、语音合成以及自然语音处理的功能，实现语音采集、录制、语音转文字、智能交互、文字转语音最好播放出来。
+#本样例代码使用baidu在线语音识别、语音合成以及自然语音处理的功能，实现语音采集、录制、语音转文字、智能交互、文字转语音最后播放出来。
 #本程序在python2.7以及win10环境下测试通过
 #完成单轮聊天机器人功能。欢迎大家修改优化！
 
